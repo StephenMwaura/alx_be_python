5de4ee251c334f4615ef549653b81a6e94d8df66
Stephen Mwaura <stevecartel44@gmail.com>
2024-06-22 21:46:32 +0300

    deleted file

--- basic_operations.py
+++
@@ -1,8 +0,0 @@
-number1 = 10
-number2 = 5
-sum_result = number1 + number2
-subtraction_result = number1 - number2
-product_result= number1 * number2
-print(f"Addition of {number1} and {number2} is: {sum_result}")
-print(f"subtraction of {number1} and {number2} is : {subtraction_result}")
-print(f"Multiplication of {number1} and {number2} is: {product_result}")
