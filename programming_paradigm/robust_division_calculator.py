0e99893622c10c14b6f86ea97b0ced83cb53c25d
Stephen Mwaura <stevecartel44@gmail.com>
2024-07-14 17:36:13 +0300

    added division file

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,15 @@
+def safe_divide(numerator, denominator):
+    try:
+        numerator = float(numerator)
+        denominator = float(denominator)
+        if denominator != 0:
+            result =numerator / denominator
+            return f"The result of the division of {numerator} / {denominator} is {result}"
+        else:
+            return f"Error: Cannot divide by zero."
+    except ValueError:
+        return f"Error: Please enter numeric values only"
+
+    
+print(safe_divide(numerator="", denominator=""))
+    
