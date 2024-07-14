a5322a77e0e416d6bbc6f65af2d5e64155d5b10f
Stephen Mwaura <stevecartel44@gmail.com>
2024-07-14 19:02:49 +0300

    corrected the simple calculator error

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -2,11 +2,15 @@ import unittest
 from simple_calculator import SimpleCalculator
 
 class TestSimpleCalculator(unittest.TestCase):
+
     def setUp(self):
-        self.calc = SimpleCalculator
+        """Set up the SimpleCalculator instance before each test."""
+        self.calc = SimpleCalculator()
+
     def test_addition(self):
-        self.assertEqual(self.calc.add(2,3),5)
-        self.assertEqual(self.calc.add(-2,2), 0)
+        """Test the addition method."""
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
     def test_subtraction(self):
         self.assertEqual(self.calc.subtract|(9,4), 5)
     def test_multiplication(self):
@@ -15,3 +19,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.divide(10,5),2)
         with self.assertRaises(ZeroDivisionError):
             (self.calc.divide(5,0),0)
+
+
+
+
