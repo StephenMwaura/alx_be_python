983df6b8fab8b10bec8b4f92900c88cbfbb965fd
Stephen Mwaura <stevecartel44@gmail.com>
2024-07-14 19:04:31 +0300

    corrections

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -14,7 +14,7 @@ class TestSimpleCalculator(unittest.TestCase):
     def test_subtraction(self):
         self.assertEqual(self.calc.subtract|(9,4), 5)
     def test_multiplication(self):
-        self.assertEqual(self.test_multiply(6,6),36)
+        self.assertEqual(self.calc.multiply(6,6),36)
     def test_division(self):
         self.assertEqual(self.calc.divide(10,5),2)
         with self.assertRaises(ZeroDivisionError):
