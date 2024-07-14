6e2a5a6c08ba8b67b64727f159702cb71f9921a9
Stephen Mwaura <stevecartel44@gmail.com>
2024-07-14 16:31:50 +0300

    adding the bank account file

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,18 @@
+class BankAccount():
+    def __init__(self, initial_balance=0):
+        self._account_balance = initial_balance
+    def deposit(self,amount ):
+        if amount > 0:
+          self._account_balance += amount
+        
+    def withdraw(self,amount):
+        if self._account_balance >= amount:
+            self._account_balance -= amount
+            return True
+        else:
+            return False
+    def display_balance(self):
+        return  f"The balance remaining is {self._account_balance}"
+    def __str__(self):
+        return f"Bankaccount(balance={self._account_balance})"
+     
